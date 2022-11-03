e2a030c4c0acd2f4b14bd32e61f5c480712b9ad3
Ali Rahman <alirahman@Alis-MBP.lan>
2022-11-02 18:47:40 -0700

    Add backend server to avoid cors error

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
+import React, { useState } from "react";
 
 const api = {
   base: "https://api.open-meteo.com/v1/forecast",
 };
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(
+        `${api.base}?latitude=34.05&longitude=-118.24&hourly=temperature_2m`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
   const dateBuilder = (d) => {
     let months = [
       "January",
@@ -46,6 +64,9 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           ></input>
         </div>
         <div className="location-box">
